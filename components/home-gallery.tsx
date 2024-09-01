63d916d1e6de660a93d94fd8fd5e306f25536594
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-09-01 19:54:13 -0400

    heihgt update

--- components/home-gallery.tsx
+++ components/home-gallery.tsx
@@ -102,84 +102,84 @@ export function HomeGallery({gallery}: Props){
 
     return(
         isSanityStudio? "": 
-            <div id="gallery" className={`relative flex justify-center items-center w-screen h-screen ${selectedProject? "blur-3xl": ""}`}>
-                <h2 className={`${blurClass} backdrop-brightness-[.8] px-2 absolute top-5 z-20 h-fit w-fit`}>Featured Projects</h2>
+            <div id="gallery" className={`relative flex justify-center items-center w-screen h-[100dvh] ${selectedProject? "blur-3xl": ""}`}>
+                <h2 className={`${blurClass} backdrop-brightness-[.8] px-2 absolute top-5 h-fit w-fit`}>Featured Projects</h2>
                 <span className="relative z-0 flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory">
-                        <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[2].projects)}}>
-                            <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[2].projects.name}</h2>
-                            <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
-                                {gallery[2].projects.roles?.map((role:any)=>{
-                                    return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
-                                })}
+                    <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[2].projects)}}>
+                        <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[2].projects.name}</h2>
+                        <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
+                            {gallery[2].projects.roles?.map((role:any)=>{
+                                return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
+                            })}
 
-                                {gallery[2].projects.collabs?.map((collab:any)=>{
-                                    return <p key={collab.name}  className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
-                                })}
+                            {gallery[2].projects.collabs?.map((collab:any)=>{
+                                return <p key={collab.name}  className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
+                            })}
 
-                                {gallery[2].projects.tags?.map((tag:any)=>{
-                                    return <p key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
-                                })}
-                            </span>
-                            <Image
-                            id="image-2"
-                            src={urlForImage(gallery[2].projects.preview).url()}
-                            alt=""
-                            width={1080}
-                            height={1080}
-                            className="w-screen h-[100dvh] object-cover shrink-0"
-                            unoptimized={urlForImage(gallery[2].projects.preview).url().includes(".gif")? true: false}
-                            />
-                        </button>
-                        <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[1].projects)}}>     
-                            <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[1].projects.name}</h2>
-                            <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
-                                {gallery[1].projects.roles?.map((role:any)=>{
-                                    return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
-                                })}
+                            {gallery[2].projects.tags?.map((tag:any)=>{
+                                return <p key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
+                            })}
+                        </span>
+                        <Image
+                        id="image-2"
+                        src={urlForImage(gallery[2].projects.preview).url()}
+                        alt=""
+                        width={1080}
+                        height={1080}
+                        className="w-screen h-[100dvh] object-cover shrink-0"
+                        unoptimized={urlForImage(gallery[2].projects.preview).url().includes(".gif")? true: false}
+                        />
+                    </button>
+                    <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[1].projects)}}>     
+                        <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[1].projects.name}</h2>
+                        <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
+                            {gallery[1].projects.roles?.map((role:any)=>{
+                                return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
+                            })}
 
-                                {gallery[1].projects.collabs?.map((collab:any)=>{
-                                    return <p key={collab.name}  className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
-                                })}
+                            {gallery[1].projects.collabs?.map((collab:any)=>{
+                                return <p key={collab.name}  className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
+                            })}
 
-                                {gallery[1].projects.tags?.map((tag:any)=>{
-                                    return <p key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
-                                })}
-                            </span>                       
-                            <Image
-                            id="image-1"
-                            src={urlForImage(gallery[1].projects.preview).url()}
-                            alt=""
-                            width={1080}
-                            height={1080}
-                            className="w-screen h-[100dvh] object-cover shrink-0"
-                            unoptimized={urlForImage(gallery[1].projects.preview).url().includes(".gif")? true: false}
-                            />
-                        </button>
-                        <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[0].projects)}}>
-                            <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[0].projects.name}</h2>
-                            <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
-                                {gallery[0].projects.roles?.map((role:any)=>{
-                                    return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
-                                })}
+                            {gallery[1].projects.tags?.map((tag:any)=>{
+                                return <p key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
+                            })}
+                        </span>                       
+                        <Image
+                        id="image-1"
+                        src={urlForImage(gallery[1].projects.preview).url()}
+                        alt=""
+                        width={1080}
+                        height={1080}
+                        className="w-screen h-[100dvh] object-cover shrink-0"
+                        unoptimized={urlForImage(gallery[1].projects.preview).url().includes(".gif")? true: false}
+                        />
+                    </button>
+                    <button className={`${parentClass}`} onClick={()=>{projectClick(gallery[0].projects)}}>
+                        <h2 className={`${blurClass} absolute top-14 lg:text-5xl text-3xl px-1 backdrop-brightness-[.8]`}>{gallery[0].projects.name}</h2>
+                        <span className={`absolute bottom-12 flex justify-center items-center flex-wrap w-screen`}>
+                            {gallery[0].projects.roles?.map((role:any)=>{
+                                return <p key={role.name} className={`${blurClass} px-1 backdrop-brightness-[.4] `}>{role.name}</p>
+                            })}
 
-                                {gallery[0].projects.collabs?.map((collab:any)=>{
-                                    return <p key={collab.name} className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
-                                })}
+                            {gallery[0].projects.collabs?.map((collab:any)=>{
+                                return <p key={collab.name} className={`${blurClass} backdrop-brightness-[.6] `}>{collab.name}</p>
+                            })}
 
-                                {gallery[0].projects.tags?.map((tag:any)=>{
-                                    return <p  key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
-                                })}
-                            </span>                                
-                            <Image
-                            id="image-0"
-                            src={urlForImage(gallery[0].projects.preview).url()}
-                            alt=""
-                            width={1080}
-                            height={1080}
-                            className="w-screen h-[100dvh] object-cover shrink-0"
-                            unoptimized={urlForImage(gallery[0].projects.preview).url().includes(".gif")? true: false}
-                            />
-                        </button>
+                            {gallery[0].projects.tags?.map((tag:any)=>{
+                                return <p  key={tag.name} className={`${blurClass} px-1 backdrop-brightness-[.8] `}>{tag.name}</p>
+                            })}
+                        </span>                                
+                        <Image
+                        id="image-0"
+                        src={urlForImage(gallery[0].projects.preview).url()}
+                        alt=""
+                        width={1080}
+                        height={1080}
+                        className="w-screen h-[100dvh] object-cover shrink-0"
+                        unoptimized={urlForImage(gallery[0].projects.preview).url().includes(".gif")? true: false}
+                        />
+                    </button>
                 </span>
             </div>
     )
