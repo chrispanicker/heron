2719138abcab4cb548f76dcb4629630f4f369feb
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-10-03 07:43:02 -0400

    simplify api

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,26 +2,15 @@ import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  console.log('Revalidation request received')  
-//   const secret = request.nextUrl.searchParams.get('secret')
-  const secret = process.env.REVALIDATION_SECRET
-  console.log('Expected secret:', secret);
-  console.log('Received secret:', secret);
-
+  const secret = request.nextUrl.searchParams.get('secret')
+  
   if (secret !== process.env.REVALIDATION_SECRET) {
-    console.log('Invalid secret provided')
     return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
   }
   
   const path = request.nextUrl.searchParams.get('path') || '/'
-  console.log(`Revalidating path: ${path}`)
-//   testing
-  try {
-    revalidatePath(path)
-    console.log('Revalidation successful')
-    return NextResponse.json({ revalidated: true, now: Date.now() })
-  } catch (err) {
-    console.error('Revalidation failed:', err)
-    return NextResponse.json({ message: 'Error revalidating' }, { status: 500 })
-  }
+  
+  revalidatePath(path)
+  
+  return NextResponse.json({ revalidated: true, now: Date.now() })
 }
