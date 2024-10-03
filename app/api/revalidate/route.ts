3e38bf4135ee223f29a82c9069df84c0359f757e
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2024-10-03 07:02:26 -0400

    more logs to console

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -2,10 +2,11 @@ import { revalidatePath } from 'next/cache'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
-  console.log('Revalidation request received')
-  
+  console.log('Revalidation request received')  
   const secret = request.nextUrl.searchParams.get('secret')
-  
+  console.log('Expected secret:', process.env.REVALIDATION_SECRET);
+  console.log('Received secret:', secret);
+
   if (secret !== process.env.REVALIDATION_SECRET) {
     console.log('Invalid secret provided')
     return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
