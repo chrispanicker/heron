6adf039670acef20495fe5ff4c9950e2231c76be
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-02-17 17:09:45 -0500

    home button fix

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -3,6 +3,7 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { TestFilter } from "./test-filter";
 import { openFilters } from "./functions";
+import { useCallback } from "react";
 
 
 type job ={
@@ -16,7 +17,40 @@ type job ={
 export function SiteHeader(info:any){
     const pathname = usePathname(); 
     const isSanityStudio = pathname.startsWith('/admin'); 
+    const router = useRouter();
+    const searchParams = useSearchParams(); 
+    const selectedProject = searchParams.get("project")
+
     const e = 1
+   const createQueryString = useCallback(
+          (name: string, value: string) => {
+              let params;
+              let stringSearchParams = searchParams.toString()
+              stringSearchParams = stringSearchParams.replaceAll("+", " ")
+              params = new URLSearchParams(stringSearchParams)
+  
+          
+              if(name==="project"){
+                  if(stringSearchParams.includes(`${name}=${value}`)){ 
+                      params.delete(name, value)
+                  }else{params.set(name, value)}
+              //not a project?? IE tags?
+              }else {
+                  if(name==="roles"||name==="tags"||name==="collabs" && selectedProject){
+                      var rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+                      params.delete("project", selectedProject!)
+                      window.scrollTo(scrollX, 0)
+                  }
+                  if(stringSearchParams.includes(`${name}=${value}`)){
+                      params.delete(name, value)
+                  }else{params.append(name, value)}
+              }
+                  
+              return params.toString()
+  
+          },
+          [searchParams]
+      )
 
     return (
         isSanityStudio? "" : 
@@ -24,8 +58,10 @@ export function SiteHeader(info:any){
             <span className="flex justify-between items-center lg:px-5 outline outline-gray-300 px-2 w-[100dvw] mono-book"
             >
                 <h1 className="flex duration-500 text-[1.35rem] sans" 
-                // onClick={()=>{document.querySelector("#foot")?.scrollIntoView({behavior:"smooth", block: "start"})}}
-                ><a href="./">Drew Litowitz&nbsp;</a>
+                onClick={()=>{
+                  document.querySelector("#foot")?.scrollIntoView({behavior:"smooth", block: "start"})
+                  router.push("?"+createQueryString("project", `${selectedProject}`), {scroll:false})
+                }}>Drew Litowitz&nbsp;
                     <p className="lg:inline-block hidden">{info.info[0].header}</p>
                 </h1>
                 <div className="flex justify-center items-center h-max">
