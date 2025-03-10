570617b722a047fe7c6782629897996c76c6a80f
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-03-10 16:37:33 -0400

    filter sizing fix on desktop

--- components/functions.ts
+++ components/functions.ts
@@ -7,18 +7,16 @@ export const openFilters = (e:number) =>{
         hed?.classList.replace("max-h-[2.4rem]","lg:max-h-[7rem]")
         hed?.classList.add("max-h-[100vh]", "overflow-x-hidden")
         hed?.classList.replace("overflow-hidden","overflow-y-scroll")
-        filters.classList.replace("h-0","h-[4rem]")
-        filters.classList.add("py-2")
+        filters.classList.replace("h-0","h-[3.8rem]")
         filters?.classList.replace("overflow-hidden", "overflow-x-hidden")
         plus?.classList.add("rotate-[45deg]")
-    }else if(filters?.classList.contains("h-[4rem]") && e===1){
+    }else if(filters?.classList.contains("h-[3.8rem]") && e===1){
         hed?.classList.replace("lg:max-h-[7rem]","max-h-[2.4rem]")
         hed?.classList.remove("max-h-[100vh]", "overflow-x-hidden")
         hed?.classList.replace("overflow-y-scroll","overflow-hidden")
-        filters?.classList.replace("h-[4rem]","h-0")
+        filters?.classList.replace("h-[3.8rem]","h-0")
         filters?.classList.replace("overflow-x-hidden","overflow-hidden")
         hed?.classList.replace("overflow-x-hidden","overflow-hidden")
-        filters?.classList.remove("py-2")
         plus?.classList.remove("rotate-[45deg]")
     }
     
