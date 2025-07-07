08a8b017ab80839dcdaf94752a0f3a61f6d72c0b
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-07-06 23:21:50 -0400

    text size for foot

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -26,7 +26,7 @@ export function SiteFooter({info, jobs}:Props){
         isSanityStudio? "" : 
         <footer id="footer" ref={footerRef} className="border-t-[2px] mt-[2px] border-black relative z-10 lg:pt-[8rem] lg:min-h-auto">
             <div className={`h-fit lg:pb-4 lg:pt-4 pb-[20dvh] pt-2 text-left grid lg:grid-cols-2 lg:mx-5 lg:text-black lg:text-[1.5rem] text-[1rem] text-gray-300  lg:leading-[1.95rem] leading-[1.6rem]`}>
-                <div className="lg:pb-0 pb-5 lg:w-auto w-screen lg:pr-6 pr-10 pl-2 serif text-[1.2rem] text-left">
+                <div className="lg:pb-0 pb-5 lg:w-auto w-screen lg:pr-6 pr-10 pl-2 serif lg:text-[1.5rem] lg:leading-[1.95rem] text-[1.2rem] text-left">
                     <PortableText value={info[0]?.bio[0]}/>
                 </div>
                 <div className={`lg:h-fit lg:pt-0 sans px-2`}>
@@ -34,7 +34,7 @@ export function SiteFooter({info, jobs}:Props){
                         <span key={`${job.company}`} className="grid grid-cols-3">
                               <div className="flex larger:flex-row flex-col w-full col-span-2">
                                 <p className="whitespace-nowrap pr-2">{job.company}</p>                
-                                <p className={`mono-book text-[1rem] uppercase mt-[.15rem] whitespace-nowrap larger:leading-[1.95rem] leading-[.2rem] larger:mb-[0rem] mb-[1.6rem]`}>{job.title}</p>
+                                <p className={`mono-book text-[1rem] uppercase larger:mt-auto mt-[.15rem] whitespace-nowrap larger:leading-[1.95rem] leading-[.2rem] larger:mb-[0rem] mb-[1.6rem]`}>{job.title}</p>
                               </div>
                             <p className={`text-right w-auto text-right serif`}>{job.years}</p>
                         </span>
