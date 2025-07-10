e01b76f91f39b3f9ce39358203ee9e33fb114661
chrispanicker <74611709+chrispanicker@users.noreply.github.com>
2025-07-09 20:47:23 -0400

    mobile font size tweaks

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -25,8 +25,8 @@ export function SiteFooter({info, jobs}:Props){
     return (
         isSanityStudio? "" : 
         <footer id="footer" ref={footerRef} className="border-t-[2px] mt-[2px] border-black relative z-10 lg:pt-[8rem] lg:min-h-auto">
-            <div className={`h-fit lg:pb-4 lg:pt-4 pb-[20dvh] pt-2 text-left grid lg:grid-cols-2 lg:mx-5 lg:text-black lg:text-[1.5rem] text-[1rem] text-gray-300  lg:leading-[1.95rem] leading-[1.6rem]`}>
-                <div className="lg:pb-0 pb-5 lg:w-auto w-screen lg:pr-6 pr-10 pl-2 serif lg:text-[1.5rem] lg:leading-[1.95rem] text-[1.2rem] text-left">
+            <div className={`h-fit lg:pb-4 lg:pt-4 pb-[30vh] pt-2 text-left grid lg:grid-cols-2 lg:mx-5 lg:text-black lg:text-[1.5rem] text-[1rem] text-gray-300  lg:leading-[1.95rem] leading-[1.6rem]`}>
+                <div className="lg:pb-0 pb-12 pt-2 lg:w-auto w-screen lg:pr-6 pr-10 pl-2 serif lg:text-[1.5rem] lg:leading-[1.95rem] text-[1.2rem] text-left">
                     <PortableText value={info[0]?.bio[0]}/>
                 </div>
                 <div className={`lg:h-fit lg:pt-0 sans px-2`}>
